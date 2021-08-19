f7620b3f3ebc520a6f54d323c078d449b60fd861
Xavries <82704808+Xavries@users.noreply.github.com>
2021-08-20 01:12:31 +0300

    edited Hello_world_PM for task 2 v0

--- Hello_world_PM.py
+++ Hello_world_PM.py
@@ -1,7 +1,55 @@
 import json
+from pyairtable import Table
+import time
+
+def sort_key(key):
+    '''
+    a func to give sort(key=) a key as a value of ID from the table for 
+    correct sorting, because pyairtable sort it by some another way
+    '''
+    return key['ID']
+
 
 def lambda_handler(event, context):
+    
+       # empty list to contain and sort data after
+    table_values = []
+    # define class from pyairtable Table('apikey', 'base_id', 'table_name')
+    table = Table('keybQzHGFofRaGHfA', 'appDzh3RGwOHlRi8U', 'MainTable')
+
+    for i in table.all():
+        # we take only i['fields'], because we don't need stuff around it
+        table_values.append(i['fields'])
+        print(i['fields'])
+
+    table_values.sort(key=sort_key)
+    print(table_values)
+
+    lich = 0
+
+    while True:
+        val_3 = 0
+        my_response = ''
+
+        while val_3 < 3:
+            if lich < len(table_values):
+                #print(table_values[lich]['ID'], table_values[lich]['title'], 'lich', lich, val_3, 'if 1')
+                my_response += table_values[lich]['title'] + '\n'
+                lich += 1
+                val_3 += 1
+
+            if lich >= len(table_values) and val_3 < 3:
+                lich = 0
+                #print(table_values[lich]['ID'], table_values[lich]['title'], 'lich', lich, val_3, 'if 2')
+                my_response += table_values[lich]['title'] + '\n'
+                lich += 1
+                val_3 += 1
+
+        print(my_response)
+        print('\n', '---Sleep 1 sec', '\n')
+        time.sleep(1)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World!')
+        'body': json.dumps(my_response)
     }
