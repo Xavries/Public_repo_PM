9fdc6f02e7a928e05176dffeb5249d5427f0a715
Xavries <82704808+Xavries@users.noreply.github.com>
2021-08-24 08:40:50 +0300

    lambda_hendler updated

--- Hello_world_PM.py
+++ Hello_world_PM.py
@@ -1,6 +1,5 @@
 import json
 from pyairtable import Table
-import time
 
 def sort_key(key):
     '''
@@ -12,42 +11,36 @@ def sort_key(key):
 
 def lambda_handler(event, context):
     
-       # empty list to contain and sort data after
+    # empty list to contain and sort data after
     table_values = []
     # define class from pyairtable Table('apikey', 'base_id', 'table_name')
+    # these values should be encrypted or uploaded from secure source
     table = Table('keybQzHGFofRaGHfA', 'appDzh3RGwOHlRi8U', 'MainTable')
 
     for i in table.all():
         # we take only i['fields'], because we don't need stuff around it
         table_values.append(i['fields'])
-        print(i['fields'])
+        #print(i['fields'])
 
     table_values.sort(key=sort_key)
-    print(table_values)
+    #print(table_values)
 
-    lich = 0
+    lich = int(event["rawQueryString"])
 
-    while True:
-        val_3 = 0
-        my_response = ''
+    val_3 = 0
+    my_response = {}
 
-        while val_3 < 3:
-            if lich < len(table_values):
-                #print(table_values[lich]['ID'], table_values[lich]['title'], 'lich', lich, val_3, 'if 1')
-                my_response += table_values[lich]['title'] + '\n'
-                lich += 1
-                val_3 += 1
+    while val_3 < 3:
+        if lich < len(table_values):
+            my_response[lich] = table_values[lich]['title']
+            lich += 1
+            val_3 += 1
 
-            if lich >= len(table_values) and val_3 < 3:
-                lich = 0
-                #print(table_values[lich]['ID'], table_values[lich]['title'], 'lich', lich, val_3, 'if 2')
-                my_response += table_values[lich]['title'] + '\n'
-                lich += 1
-                val_3 += 1
-
-        print(my_response)
-        print('\n', '---Sleep 1 sec', '\n')
-        time.sleep(1)
+        if lich >= len(table_values) and val_3 < 3:
+            lich = 0
+            my_response[lich] = table_values[lich]['title']
+            lich += 1
+            val_3 += 1
     
     return {
         'statusCode': 200,
